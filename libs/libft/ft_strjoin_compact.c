6f4f0c8d9176a184b7a412ecd6f331aaf0c03abd
Christian Meng <cmeng@1-D-24.42heilbronn.de>
2023-02-10 13:06:38 +0100

    libs included

---
+++ libs/libft/ft_strjoin_compact.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_compact.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 13:36:45 by cmeng             #+#    #+#             */
+/*   Updated: 2023/02/10 10:31:35 by cmeng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// edited: '\n' overwritten with src[ft_strlen(src) - 1] = 0
+
+int	ft_strjoin_compact(char **dst, char *src)
+{
+	char	*new;
+	int		len;
+
+	len = ft_strlen(src);
+	src[len] = 0;
+	new = ft_strjoin(*dst, src);
+	free(*dst);
+	free(src);
+	*dst = new;
+	return (new == NULL);
+}
--- src/main.c
+++ src/main.c
@@ -6,13 +6,39 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 13:10:35 by cmeng             #+#    #+#             */
-/*   Updated: 2023/02/09 13:11:04 by cmeng            ###   ########.fr       */
+/*   Updated: 2023/02/10 11:44:24 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/push_swap.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	int		i;
+	// int		j;
+	char	**split;
+	char	*tmp;
 
+	tmp = NULL;
+	split = NULL;
+	// if (ft_calloc2(1, 1, (void *) &tmp))
+	// 	return (1);
+	if (argc < 2)
+		return (1);
+	i = 1;
+	// j = 0;
+	while (i < argc)
+	{
+		tmp = ft_strtrim(argv[i], " ");
+		// ft_printf("%s\n", tmp);
+		split = ft_split(argv[i], ' ');
+		while (*split != NULL)
+			ft_printf("%s\n", *split++);
+		// if (ft_strjoin_compact(input, tmp))
+		// 	return (1);
+		i++;
+	}
+	ft_printf("%s\n", argv[i]);
 	return (0);
 }
+
