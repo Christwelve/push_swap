62ffd054d9e0c346880c03264be757090f604389
Christian Meng <cmeng@1-C-24.42heilbronn.de>
2023-03-21 16:46:15 +0100

    modified swap functions

--- src/swap.c
+++ src/swap.c
@@ -6,13 +6,13 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:37:46 by cmeng             #+#    #+#             */
-/*   Updated: 2023/03/16 17:52:31 by cmeng            ###   ########.fr       */
+/*   Updated: 2023/03/21 16:35:55 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	swap(stack_t stack)
+void	swap(t_circle stack)
 {
 	int	first;
 
@@ -21,19 +21,19 @@ void	swap(stack_t stack)
 	add_element(stack, first, 1);
 }
 
-void	sa(stack_t stack)
+void	sa(t_circle stack)
 {
 	swap(stack);
 	ft_printf("%s", "sa");
 }
 
-void	sb(stack_t stack)
+void	sb(t_circle stack)
 {
 	swap(stack);
 	ft_printf("%s", "sb");
 }
 
-void	ss(stack_t stack_a, stack_t stack_b)
+void	ss(t_circle stack_a, t_circle stack_b)
 {
 	swap(stack_a);
 	swap(stack_b);
