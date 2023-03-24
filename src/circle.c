7446920b7ba814bcee1f0e2a34f64089bc16344f
Christian Meng <christianmeng@christians-air.lan>
2023-03-25 00:44:10 +0100

    fixed create_stack

--- src/circle.c
+++ src/circle.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   circle.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
+/*   By: christianmeng <christianmeng@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 16:46:27 by cmeng             #+#    #+#             */
-/*   Updated: 2023/03/23 21:07:43 by cmeng            ###   ########.fr       */
+/*   Updated: 2023/03/25 00:43:19 by christianme      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	create_stack(t_circle *stack, size_t size)
 {
-	if (ft_calloc2(size, sizeof(int), (void **)&stack))
+	if (ft_mallocp(size * sizeof(int), (void**) &stack->elements))
 		return (1);
 	stack->size = 0;
 	stack->start = 0;
@@ -27,13 +27,13 @@ unsigned int	calc_index(t_circle *stack, long index)
 	return ((index + stack->start + stack->max_size) % stack->max_size);
 }
 
-void	add_element(t_circle *stack, int element, unsigned long index)
+void	add_element(t_circle *stack, int element, long index)
 {
 	index = calc_index(stack, index);
 	stack->elements[index] = element;
 }
 
-int	get_element(t_circle *stack, unsigned long index)
+int	get_element(t_circle *stack, long index)
 {
 	index = calc_index(stack, index);
 	return (stack->elements[index]);
